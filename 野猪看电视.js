2f32ba3957f162f1f71ceff080f6e812312df6ae
frozenthrone <moon.zju@gmail.com>
2018-09-28 15:08:21 +0800

    Update 野猪看电视.js

--- 野猪看电视.js
+++ 野猪看电视.js
@@ -2,10 +2,11 @@
 moonzju@twitter
 野猪看电视
 观看电视直播源的节目，外国电视为主，绝大部分是720P画质，某些频道可能需要翻墙才能观看
-version：1.0.1
+感谢LCO LOK网友提供的直接点击播放的方法
+version：1.0.2
 */ 
 
-var channelName=["TVB翡翠台", "TVB财经台","TVBS HD","NHK", "CNN", "ABC News", "Weather" ,"MSNBC","中国气象频道"]
+var channelName=["TVB翡翠台", "TVB财经台","TVBS HD","NHK", "CNN", "ABC News", "Weather" ,"MSNBC","中国气象频道","凤凰卫视中文台","凤凰卫视资讯台","凤凰卫视香港台"]
 var channelAddr=[
     "http://m.567it.com/jade.m3u8",
     "http://e1.vdowowza.vip.hk1.tvb.com/tvblive/smil:mobilehd_financeintl.smil/chunklist.m3u8",
@@ -15,49 +16,82 @@ var channelAddr=[
     "https://abclive2-lh.akamaihd.net/i/abc_live11@423404/master.m3u8",
     "https://weather-lh.akamaihd.net/i/twc_1@92006/master.m3u8",
     "http://tvemsnbc-lh.akamaihd.net/i/nbcmsnbc_1@122532/master.m3u8",
-    "http://hls.weathertv.cn/tslslive/qCFIfHB/hls/live_sd.m3u8"
+    "http://hls.weathertv.cn/tslslive/qCFIfHB/hls/live_sd.m3u8",
+    "http://223.110.245.167/ott.js.chinamobile.com/PLTV/3/224/3221226922/index.m3u8",
+    "http://223.110.245.167/ott.js.chinamobile.com/PLTV/3/224/3221226923/index.m3u8",
+    "http://183.207.249.35/PLTV/3/224/3221226975/index.m3u8"    
 ]
 
-
-$ui.render({
-  props:{
-    title:"野猪看电视"
-  },
-  views: [
-    {
-      type: "menu",
-      props: {
-        items: channelName
-      },
-      layout: function(make) {
-        make.left.top.right.equalTo(0)
-        make.height.equalTo(44)
-      },
-      events: {
-        changed: function(sender) {
-          //var items = sender.items
-          //var index = sender.index
-          var url=channelAddr[sender.index]
-          $("player").src=url       
-          $("player").play()
-          $ui.toast(sender.index + ": " + sender.items[sender.index])
-        }
-      }
+const video = {
+    type: "video",
+    props: {
+        id:'video',
+      src: "",
+      
     },
-     
-    {
-      type: "video",
-     
-      props: {
-        id:"player",
-        src: channelAddr[0]
-      },
-      layout: function(make, view) {
-        make.left.right.equalTo(0)
-        make.top.equalTo(40)
-        make.height.equalTo(256)
+    layout: function(make, view) {
+      make.left.right.equalTo(0)
+      make.top.equalTo(0)
+      make.height.equalTo(256)
+    }
+  };
+
+
+  const menu={
+    type: "matrix",
+    props: {
+      
+      columns: 2,
+      itemHeight: 88,
+      spacing: 10,
+      template: [{
+        type: "label",
+        props: {
+          id: "tile",
+          radius:8,
+          bgcolor: $color("#474b51"),
+          textColor: $color("#abb2bf"),
+          align: $align.center,
+          font: $font(20)
+        },
+        layout: $layout.fill
+      }],
+      data: channelName.map(function(item) {
+        return {
+          tile: {
+            text: "" + item
+          }
+        }
+      })
+    },
+    layout: function(make) {
+      make.left.bottom.right.equalTo(0)
+      make.top.equalTo(280)
+      make.height.equalTo(446)
+    },
+    events: {
+      didSelect: function(sender, indexPath, data) {
+        var token = data.tile.text
+        var index = indexPath.item
+        var url=channelAddr[index]
+        
+        
+          $("video").src=url
+  
+          $delay(0.1, function() {
+            $("video").play()
+        });
+                    
+        
       }
     }
+  }
 
-  ]
-})
+$ui.render({
+    props: {
+        title: ""
+    },
+    views: [video,menu]
+});
+
+
