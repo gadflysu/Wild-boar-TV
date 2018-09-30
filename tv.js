da84a6cf1c49ba57a5b5e7770ea94c5aab4d0477
frozenthrone <moon.zju@gmail.com>
2018-09-30 17:11:26 +0800

    Update tv.js

--- tv.js
+++ tv.js
@@ -3,10 +3,10 @@ moonzju@twitter
 野猪看电视
 观看电视直播源的节目，外国电视为主，绝大部分是720P画质，某些频道可能需要翻墙才能观看
 感谢LCO LOK网友提供的直接点击播放的方法
-version：1.0.5
+version：1.0.6
 */ 
 
-var channelName=["FOX2", "FOX3","湖南卫视高清","TVB财经台","TVBS HD","NHK", "CNN", "ABC News", "Weather" ,"MSNBC","中国气象频道","凤凰卫视中文台","凤凰卫视资讯台","凤凰卫视香港台","高清时装台","TVB娱乐新闻台","奇妙电视","金鹰纪实高清"]
+var channelName=["FOX2", "FOX3","湖南卫视高清","TVB财经台","TVBS HD","NHK", "CNN", "ABC News", "Weather" ,"MSNBC","中国气象频道","凤凰卫视中文台","凤凰卫视资讯台","凤凰卫视香港台","高清时装台","TVB娱乐新闻台","奇妙电视","金鹰纪实高清","USA DW","DTV HD","BTV HD","MTV HD"]
 var channelAddr=[
     "http://cdn2.bear-tv.com/freetv/TOT_foxsport2/chunks.m3u8?",
     "http://cdn2.bear-tv.com/freetv/TOT_foxsport3/chunks.m3u8?",
@@ -25,7 +25,11 @@ var channelAddr=[
     "http://wfc.bonus-tv.ru:80/cdn/wfcint/tracks-v2a1/index.m3u8",
     "http://token.tvb.com/stream/live/enews/hls/mobilehd_enews.smil",
     "http://media.fantv.hk/m3u8/archive/channel2_stream1.m3u8",
-    "http://223.110.243.148/PLTV/4/224/3221227490/index.m3u8"
+    "http://223.110.243.148/PLTV/4/224/3221227490/index.m3u8",
+    "http://dwstream4-lh.akamaihd.net/i/dwstream4_live@131329/master.m3u8#EXTINF:-1,USA",
+    "http://media4.tripsmarter.com:1935/LiveTV/DTVHD/playlist.m3u8",
+    "http://media4.tripsmarter.com:1935/LiveTV/BTVHD/chucklist.m3u8",
+    "http://media4.tripsmarter.com:1935/LiveTV/MTVHD/playlist.m3u8"
 ]
 
 const video = {
